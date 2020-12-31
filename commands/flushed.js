961ca69aa144588e469e9f01b8f75596046a6e5c
Ryan <4993641-cubismod@users.noreply.gitlab.com>
2020-12-31 15:43:25 -0500

    flushed command

--- commands/flushed.js
+++ commands/flushed.js
@@ -1,23 +1,23 @@
 module.exports = {
-  name: 'random',
-  description: 'Insert a random emote!',
-  usage: '[random] (optional)[number of emotes to create max: 15]',
-  aliases: ['r', 'rnd'],
+  name: 'flushed',
+  description: 'Insert a random flushed emote!',
+  usage: '[flushed] (optional)[number of emotes to create max: 15]',
+  aliases: ['flush', 'fl', 'f'],
   requiresCache: true,
   execute (message, args, client, cache) {
     require('../helper')
     const Pandemonium = require('pandemonium')
     let repeats = 1
+    const options = cache.search('fl')
     const intVers = parseInt(args[0])
     if (args.length > 0 && !isNaN(intVers) && intVers < 16) {
       repeats = intVers
     }
-    console.log('random command used')
+    console.log('flushed command used')
     const chance = Pandemonium
     let msg = ''
-    const emoteArray = cache.createEmoteArray()
     for (let i = 0; i < repeats; i++) {
-      msg = msg.concat(chance.choice(emoteArray).toString())
+      msg = msg.concat(chance.choice(options).item.toString())
     }
     message.channel.send(msg)
   }
