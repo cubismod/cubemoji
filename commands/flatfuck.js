d439f0510d751d2bc024fa52cbe1d959bc58f9cd
Ryan <4993641-cubismod@users.noreply.gitlab.com>
2020-12-31 08:36:55 -0500

    ff updated

--- commands/flatfuck.js
+++ commands/flatfuck.js
@@ -5,13 +5,21 @@ module.exports = {
   aliases: ['ff', 'flat_fuck_friday', 'femboy_friday'],
   requiresCache: false,
   execute (message) {
+    const Pandemonium = require('pandemonium')
+    const chance = Pandemonium
+    const contentOptions = [
+      'https://www.youtube.com/watch?v=A5U8ypHq3BU',
+      'https://www.youtube.com/watch?v=H0b3moDYTUA',
+      'https://www.youtube.com/watch?v=NRn8coFCIjU',
+      'https://twitter.com/GatorsDaily'
+    ]
     console.log('flat_fuck used')
     const day = new Date()
-    if (day.getDay() === 4) {
-      console.log('flat fuck command used')
-      message.channel.send(':hahagators:')
+    if (day.getDay() === 5) {
+      message.channel.send('<:hahaflatfuck:769663194729414656>')
+      message.channel.send(chance.choice(contentOptions))
     } else {
-      message.channel.send('i am sorry but no')
+      message.channel.send('sorry no, it is not flat fuck friday, nor is it femboy friday, nor is it flushed friday, nor is it flat fluck femboy flushed friday 😳')
     }
   }
 }
