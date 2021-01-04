c5eab48a35ef86723e2cb0e7692b0eb79d338d23
Ryan <4993641-cubismod@users.noreply.gitlab.com>
2021-01-03 19:04:02 -0500

    fix win condition never triggering

--- commands/slots.js
+++ commands/slots.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'Play the slots!',
   usage: '[slots]',
   aliases: ['sl'],
-  cooldown: 15,
+  // cooldown: 15,
   requiresCache: true,
   execute (message, args, client, cache) {
     // creates text representing slots
@@ -17,27 +17,35 @@ module.exports = {
       // this is a promise so we have to do some async code
       const emoteContent = await emoteMsg
       const headerContent = await headerMsg
-      const emotes = emoteContent.content.split('<')
-      // split should look like this
-      // 1, 2, 3 contain raw emote values
-      if (emotes[1] === emotes[2] === emotes[3]) {
+      const emotes = emoteContent.content.split(':')
+      // split should look something like this
+      // [
+      //   "<", 0
+      //   "amongus", 1
+      //   "780838024803254303><",  2
+      //   "triCri",  3
+      //   "778722027854888991><a", 4
+      //   "flushWide", 5
+      //   "783113008129245215>", 6
+      // ]
+      if ((emotes[1] === emotes[3]) &&
+      (emotes[3] === emotes[5])) {
         // user want
         headerContent.edit('**Congrats you won!**')
-      }
-      else {
+      } else {
         headerContent.edit('**Sorry you lost!**')
       }
     }
     const pand = require('pandemonium')
     const emoteArray = cache.createEmoteArray()
     // get slot options
-    const slotOptions = pand.geometricReservoirSample(15, emoteArray)
+    const slotOptions = pand.geometricReservoirSample(20, emoteArray)
     const headerMsg = message.channel.send('**Your slots <a:dieRoll:795419079254605834>**')
     const slotsMsg = message.channel.send(createSlotText(slotOptions))
     // edit with the options for 5 times
     slotsMsg.then((sentMsg) => {
-      for (let i=1; i < 5; i++) {
-        setTimeout(editMsg, 500*i, sentMsg)
+      for (let i = 1; i < 5; i++) {
+        setTimeout(editMsg, 500 * i, sentMsg)
       }
       // last frame we decide on a result
       // after 3000 ms
