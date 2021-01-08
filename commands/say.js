e8fc0a45c8cf602a0c4184bde802235339fa4aa6
Ryan <4993641-cubismod@users.noreply.gitlab.com>
2021-01-07 23:27:13 -0500

    stupid say feature

---
+++ commands/say.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: 'say',
+  description: 'Say something',
+  usage: '[say] <channel id> <text>',
+  aliases: ['sy'],
+  cooldown: 5,
+  requiresCache: true,
+  execute (message, args, client, cache) {
+    console.log('add_flush used')
+    if (args.length < 2 || message.author.id !== '170358606590377984') {
+      message.reply('You must include a channel id and text in the command and be cubis!')
+    } else {
+      const channel = client.channels.fetch(args[0])
+      channel.then(fetched => {
+        // get the text and put it back together
+        const text = args.slice(1).join(' ')
+        fetched.send(text)
+      })
+        .catch(reason => {
+          console.log(reason)
+          message.reply('unable to retrieve the channel')
+        })
+    }
+  }
+}
