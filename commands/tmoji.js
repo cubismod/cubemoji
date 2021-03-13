33edce2e802377df57a162ad8808b3c9bc475649
Ryan <4993641-cubismod@users.noreply.gitlab.com>
2021-03-12 19:23:38 -0500

    search updated in tmoji

--- commands/tmoji.js
+++ commands/tmoji.js
@@ -3,15 +3,18 @@ module.exports = {
   name: 'tmoji',
   description: 'Replaces the string you send with the emote versions. If an emote is not matched, then it won\'t result in an emote being printed.',
   usage: 'tmoji <emote_name> <emote_name> ...',
+  aliases: ['tm'],
   execute (message, args, client, helper) {
     const emList = []
     args.forEach(element => {
       // do a search for each arg
       const res = helper.cache.search(element)
       if (res.length > 0) {
-        // found a match
-        const match = Pandemonium.choice(res)
-        emList.push(match.item)
+        if (res[0].item.name !== element.toLowerCase()) {
+          // pick a random match if we don't have an exact match
+          const match = Pandemonium.choice(res)
+          emList.push(match.item)
+        } else emList.push(res[0].item)
       }
     })
     if (emList.length > 0 && emList.length < 41) {
