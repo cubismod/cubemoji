a8dd3d8972f90a8907fe465af5e364248ac667b0
Ryan <4993641-cubismod@users.noreply.gitlab.com>
2021-09-23 10:15:32 -0400

    more basics

--- src/Main.ts
+++ src/Main.ts
@@ -1,19 +1,26 @@
 /* eslint-disable node/no-path-concat */
 import 'reflect-metadata'
+import { Intents } from 'discord.js'
 import { Client } from '@typeit/discord'
 import secrets from '../secrets.json'
+import pkginfo from '../package.json'
 
 async function start () {
   const client = new Client({
+    intents: [
+      Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+    ],
     classes: [
       `${__dirname}/*Discord.ts`, // glob string to load the classes
       `${__dirname}/*Discord.js` // If you compile using "tsc" the file extension change to .js
     ],
-    silent: false,
-    variablesChar: 'd!'
+    silent: false
   })
 
   await client.login(secrets.token)
+  console.log(`cubemoji ${pkginfo.version} is now running...`)
 }
 
 start()
