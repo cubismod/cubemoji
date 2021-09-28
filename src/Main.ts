862877f50069511174b4fce2e4ad6498584b9632
Ryan <4993641-cubismod@users.noreply.gitlab.com>
2021-09-27 21:22:29 -0400

    info completed

--- src/Main.ts
+++ src/Main.ts
@@ -20,7 +20,9 @@ export class Main {
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_PRESENCES
       ],
       classes: [
         `${__dirname}/**/*.{js,ts}` // glob string to load the classes
--- src/discords/commands/info.ts
+++ src/discords/commands/info.ts
@@ -1,7 +1,7 @@
 // https://github.com/oceanroleplay/discord.ts-example/blob/main/src/commands/slashes.ts
-import { CommandInteraction, User, MessageEmbed } from 'discord.js'
+import { CommandInteraction, MessageEmbed, GuildMember } from 'discord.js'
 import { Discord, Slash, SlashOption } from 'discordx'
-import { Companion } from '../../Cubemoji'
+import { Companion, Source } from '../../Cubemoji'
 
 @Discord()
 export abstract class Info {
@@ -11,37 +11,80 @@ export abstract class Info {
   async info (
     @SlashOption('emote', { description: 'an emote name or actual emote' })
       emote: string,
-    @SlashOption('user', { description: 'a mention of a user' })
-      user: User,
+    @SlashOption('member', { description: 'a mention of a server member' })
+      member: GuildMember,
       interaction: CommandInteraction
   ) {
     const companion : Companion = globalThis.companion
     // check our args
-    if (emote != null) {
-      await interaction.deferReply()
+    if (emote !== undefined) {
       // emote parsing code
       const emoteName = emote.toLowerCase()
       const res = companion.cache.retrieve(emoteName)
       if (res != null) {
+        try {
+          await interaction.deferReply()
+        } catch (err) {
+          console.error(err)
+        }
         const embed = new MessageEmbed()
-        embed.color = 7738070
+        embed.setColor('RANDOM')
         embed.setImage(res.url)
         embed.setTitle(res.name)
         switch (res.source) {
-          case 0: {
+          case Source.Discord: {
             if (res.guildEmoji?.createdAt) embed.addField('Creation Date', res.guildEmoji.createdAt.toLocaleString())
             if (res.guildEmoji?.id) embed.addField('ID', res.guildEmoji.id)
             embed.addField('URL', res.url)
-            if (res.guildEmoji?.animated) embed.addField('Animated', res.guildEmoji.animated.toString())
+            if (res.guildEmoji?.animated) embed.addField('Animated', 'Yes')
             if (res.guildEmoji?.guild.name) embed.addField('Origin Server Name', res.guildEmoji.guild.name)
             const author = await res.guildEmoji?.fetchAuthor()
             if (author !== undefined) embed.addField('Author', author.username)
-            await interaction.editReply({ embeds: [embed] })
+            try {
+              await interaction.editReply({ embeds: [embed] })
+            } catch (err) {
+              console.error(err)
+            }
+            break
+          }
+          case Source.Mutant: {
+            embed.addField('Disclaimer', ' This bot uses Mutant Standard emoji (https://mutant.tech) which are licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License (https://creativecommons.org/licenses/by-nc-sa/4.0/).')
+            try {
+              await interaction.editReply({ embeds: [embed] })
+            } catch (err) {
+              console.error(err)
+            }
           }
         }
       } else {
-        interaction.reply('Emote not found!')
-        console.log(user)
+        try {
+          await interaction.reply({ content: '**Error**: Emote not found!', ephemeral: true })
+        } catch (err) {
+          console.error(err)
+        }
+      }
+    }
+    if (member !== undefined) {
+      const avatarURL = member.user.displayAvatarURL({ format: 'png', dynamic: true, size: 256 })
+      const embed = new MessageEmbed()
+      embed.setColor('RANDOM')
+      embed.setTitle(member.user.tag)
+      embed.setImage(avatarURL)
+      embed.addField('ID', member.user.id)
+      embed.addField('Discord Join Date', member.user.createdAt.toLocaleString())
+      if (member.joinedAt) embed.addField('Server Join Date', member.joinedAt.toLocaleString())
+      embed.addField('Bot', member.user.bot.toString())
+      try {
+        await interaction.reply({ embeds: [embed] })
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    if ((member === undefined) && (emote === undefined)) {
+      try {
+        await interaction.reply({ content: '**Error**: No arguments specified', ephemeral: true })
+      } catch (err) {
+        console.error(err)
       }
     }
   }
