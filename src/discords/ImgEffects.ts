aac05cce91335847ceeb4778b2427a106998ba06
Ryan <4993641-cubismod@users.noreply.gitlab.com>
2021-10-02 16:16:13 -0400

    edit

--- src/discords/ImgEffects.ts
+++ src/discords/ImgEffects.ts
@@ -1,10 +1,12 @@
 // Image Effects like Rescale, Add Face, etc. are done in here
 import { fromFile } from 'file-type'
-import Jimp = require('jimp')
 import { random } from 'pandemonium'
 import { downloadImage } from '../CommandHelper'
 import gm = require('gm')
 import path = require('path')
+import strings from '../res/strings.json'
+
+const projDir = path.resolve('../../')
 
 // perform a liquid rescale/ seam carving on an image
 // returns the path to the image file
@@ -28,32 +30,34 @@ export async function rescale (externalUrl: string) {
   // get the file type of the image file
   const ft = await fromFile(localUrl)
   if (ft !== undefined) {
-    const filePath = path.join(path.resolve('./../../download'), `${Date.now()}.${ft.ext}}`)
+    const filePath = path.join(projDir, `download/${Date.now()}.${ft.ext}}`)
     // imagemagick only has liquid rescale, not graphicsmagick
     gm.subClass({ imageMagick: true })(localUrl)
       .out('-liquidrescale', newSize)
       .write(filePath, (err) => {
-        if (err) {
-          throw (err)
-        }
+        if (err) throw (err)
       })
     return filePath
   } else {
-    throw Error('could not determine filetype of image')
+    throw Error(strings.fileTypeUndetermined)
   }
 }
 
-export async function addFace (baseUrl: string, faceUrl: string) {
-  const flush = await Jimp.read(baseUrl)
-  const baseEmote = await Jimp.read(faceUrl)
-  flush.contain(baseEmote.bitmap.width, baseEmote.bitmap.height, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE)
-  const mask = baseEmote.clone()
-  // create a mask image to ensure shadow doesn't go over transparent regions
-  mask.grayscale()
-  mask.opaque()
-  if (faceUrl.includes('jfc')) flush.mask(mask, 0, 0)
-  baseEmote.composite(flush, 0, 0, { mode: Jimp.BLEND_SOURCE_OVER, opacityDest: 1, opacitySource: 1 })
-  return baseEmote.getBufferAsync(Jimp.AUTO.toString()).then(buf => {
-    return buf
-  })
+// add an emoji (face) to any image
+// return the file path to the edited image
+export async function addFace (baseUrl: string, face: string) {
+  // this also determines if the base image exists
+  const ft = await fromFile(baseUrl)
+  if (ft !== undefined) {
+    const filePath = path.join(projDir, `download/${Date.now()}.${ft.ext}}`)
+    gm(baseUrl)
+      // test this
+      .composite(path.join(projDir, `assets/${face}.png`))
+      .write(filePath, (err) => {
+        if (err) throw (err)
+      })
+    return filePath
+  } else {
+    throw Error(strings.fileTypeUndetermined)
+  }
 }
