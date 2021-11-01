03d3960243bff0b0157dcc48cad97406efe65a42
Ryan <4993641-cubismod@users.noreply.gitlab.com>
2021-11-01 15:54:01 -0400

    list shows emote names in menu

--- src/discords/commands/List.ts
+++ src/discords/commands/List.ts
@@ -1,5 +1,5 @@
 import { CommandInteraction } from 'discord.js'
-import { Discord, Slash } from 'discordx'
+import { Discord, Slash, SlashChoice, SlashOption } from 'discordx'
 import { Pagination } from '@discordx/utilities'
 import { grabEmoteCache } from '../../CommandHelper'
 
@@ -8,29 +8,52 @@ export abstract class List {
   @Slash('list', {
     description: 'Get a full list of emojis'
   })
-  async list (interaction: CommandInteraction) {
+  async list (
+    @SlashChoice('Discord', 'discord')
+    @SlashChoice('Mutant', 'mutant')
+    @SlashChoice('All', 'all')
+    @SlashOption('subset', { description: 'Which subset of emotes would you like to choose from?', required: true })
+      subset: string,
+      interaction: CommandInteraction) {
     const emoteCache = grabEmoteCache()
     if (emoteCache !== undefined) {
-      // 50 emotes per page
-    // ~18 pages
-      const emotes: string[] = []
-      let curEmotePage = ''
-      emoteCache.discEmojis.forEach((emote, i) => {
-        if (i !== 0 && !(i % 60)) {
-        // append page to emotes obj
-        // clear working page
-          emotes.push(curEmotePage)
-          curEmotePage = ''
-        } else if (i === emoteCache.discEmojis.length - 1) {
-        // we are done reading this
-          emotes.push(curEmotePage)
-        } else {
-          curEmotePage = curEmotePage.concat(emote.toString())
-        }
-      })
-      await new Pagination(interaction, emotes, {
-        type: 'SELECT_MENU'
-      }).send()
+      if (subset === 'discord') {
+        // 50 emotes per page
+        // ~18 pages
+        const emotes: string[] = []
+        const menuText: string[] = []
+        let menuItem = ''
+        let curEmotePage = ''
+        emoteCache.discEmojis.forEach((emote, i) => {
+          if (i !== 0 && !(i % 60)) {
+            // get the last emote that we added to the page
+            // and add to menu text
+            menuItem = menuItem.concat(emoteCache.discEmojis[i - 1].name)
+            // append page to emotes obj
+            // clear working page
+            emotes.push(curEmotePage)
+            menuText.push(menuItem)
+            curEmotePage = ''
+            menuItem = ''
+          } else if (i === emoteCache.discEmojis.length - 1) {
+            menuItem = menuItem.concat(emoteCache.discEmojis[i - 1].name)
+            // we are done reading this
+            emotes.push(curEmotePage)
+            menuText.push(menuItem)
+          } else if (emote.guildEmoji) {
+            if (curEmotePage === '') {
+              menuItem = `${emote.name} - `
+            }
+            // create each emote page
+            curEmotePage = curEmotePage.concat(emote.guildEmoji.toString())
+          }
+        })
+        await new Pagination(interaction, emotes, {
+          type: 'SELECT_MENU',
+          ephemeral: true,
+          pageText: menuText
+        }).send()
+      }
     }
   }
 }
