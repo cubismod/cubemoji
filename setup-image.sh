aa62cf3030d619850cb1c61fd611e266cf0cca01
Ryan <4993641-cubismod@users.noreply.gitlab.com>
2021-10-31 23:29:48 -0400

    once again fixing my screwups

--- setup-image.sh
+++ setup-image.sh
@@ -1,4 +1,10 @@
-#!/bin/ash
-apk update
-apk upgrade
-apk add imagemagick graphicsmagick
+#!/bin/bash
+set -euo pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get -y upgrade
+apt-get -y install apt-utils imagemagick graphicsmagick
+
+apt-get clean
+rm -rf /var/lib/apt/lists*
