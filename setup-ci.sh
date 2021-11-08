af47a826cd5fc90b4d6a2c070328df2466dbf188
Ryan <4993641-cubismod@users.noreply.gitlab.com>
2021-11-07 21:45:45 -0500

    hopefully fix

--- setup-ci.sh
+++ setup-ci.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # adds dummy secrets files so typescript will run for ci
-printf '{"token": "aba", "environment": "npr"}' > secrets.json
+printf '{"token": "aba", "environment": "npr", "testGuild": "123", "testChannel": "234"}' > secrets.json
 printf '{"type": "service_account", "project_id": "id", "private_key_id": "a", "private_key": "b", "client_email": "a", "client_id": "1", "auth_uri": "auth", "token_uri": "token", "auth_provider_x509_cert_url": "a", "client_x509_cert_url": "a"}' > serviceAccountKey.json
