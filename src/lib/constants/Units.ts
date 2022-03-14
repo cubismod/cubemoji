f620cf5b731007c0f5b0950ed4f174240f02349a
cubis <cubis-git.3gyz7@simplelogin.co>
2022-03-14 12:31:02 -0400

    fix to constant val

--- src/lib/constants/Units.ts
+++ src/lib/constants/Units.ts
@@ -1,7 +1,7 @@
 // constant exports for units
 export namespace Milliseconds {
   export const week = 6.048e+8
-  export const thirtyMin = 6.048e+8
+  export const thirtyMin = 1.8e+6
   export const fiveMin = 300000
   export const thirtySec = 30000
   export const day = 8.64e+7
