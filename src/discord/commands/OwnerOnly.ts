275c7f1003026b1f13c70a5a9f9c2d6c2698a73a
Ryan <git@hexa.mozmail.com>
2022-08-05 21:04:45 -0400

    new command

--- src/discord/commands/OwnerOnly.ts
+++ src/discord/commands/OwnerOnly.ts
@@ -1,8 +1,14 @@
-import { CommandInteraction, Guild, MessageActionRow, MessageButton } from 'discord.js';
+import { randomUUID } from 'crypto';
+import { CommandInteraction, Guild, MessageActionRow, MessageAttachment, MessageButton } from 'discord.js';
 import { ButtonComponent, Discord, Guard, Slash, SlashOption } from 'discordx';
+import { rm, writeFile } from 'fs/promises';
+import path from 'path';
 import { container } from 'tsyringe';
+import { CubeLogger } from '../../lib/observability/CubeLogger';
 import { InspectorWrapper } from '../../lib/perf/InspectorWrapper';
 import { botOwnerDetect } from '../Guards';
+
+const logger = container.resolve(CubeLogger).command;
 
 @Discord()
 @Guard(botOwnerDetect)
@@ -74,5 +80,49 @@ export abstract class PerformanceTest {
         });
     }
     await this.inspector.toggleSession();
+  }
+}
+
+@Discord()
+@Guard(botOwnerDetect)
+export abstract class PermGrab {
+  @Slash('permgrab')
+  async permGrab(interaction: CommandInteraction) {
+    // https://discord.com/developers/docs/interactions/application-commands#permissions
+    // Get a list of application.id
+    // all guild ids
+    // all command ids
+    // and put into a text document
+
+    await interaction.deferReply();
+
+    const message: string[] = [];
+
+    message.push(`Application ID: ${interaction.client.application?.id}`);
+
+    message.push('\nCommands:');
+
+    // push each guildID and all the commandIDs in each
+    for (const guild of interaction.client.guilds.cache) {
+      message.push(`\n${guild[0]}, ${guild[1].name}\n`);
+      for (const command of guild[1].commands.cache) {
+        message.push(command[0]);
+      }
+    }
+
+    // save to a file
+    const fileName = path.join('./download', randomUUID() + '.txt');
+    try {
+      await writeFile(fileName, message.join('\n'));
+    } catch (err) {
+      logger.error(err);
+      await interaction.editReply('Unable to write to file.');
+    }
+    await interaction.editReply({
+      files: [
+        new MessageAttachment(fileName)
+      ]
+    });
+    await rm(fileName);
   }
 }
